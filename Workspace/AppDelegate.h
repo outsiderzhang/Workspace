dc323b7c7ac7b96b90ac71226cab897ca254a83e
Zhang <outsider.zhangr@gmail.com>
2018-04-28 13:50:27 +0800

    Initial Commit

---
+++ Workspace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Workspace
+//
+//  Created by sanjia on 2018/4/28.
+//  Copyright © 2018年 zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
